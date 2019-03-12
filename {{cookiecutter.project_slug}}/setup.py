766e9af06cd1b3740251c4e0942747eb3a6dcfa6
Justin (:flaviusb) Marsh <justin.marsh@flaviusb.net>
2019-03-12 15:56:41 +1300

    Change namespace to cookiecutter.
    Make python project namespace = module stuff.

--- {{cookiecutter.project_slug}}/setup.py
+++ {{cookiecutter.project_slug}}/setup.py
@@ -3,25 +3,25 @@ from setuptools import setup
 classifiers = """
 Development Status :: 3 - Alpha
 Intended Audience :: Developers
-{{rvit_template.license_long}}
+{{cookiecutter.license_long}}
 Operating System :: OS Independent
 Programming Language :: Python :: 3
 """.strip().splitlines()
 
 
 setup(
-    name='{{rvit_template.project_name}}',
-    version='{{rvit_template.version}}',
+    name='{{cookiecutter.project_name}}',
+    version='{{cookiecutter.version}}',
     classifiers=classifiers,
     description='description of your app',
     url='',
-    author='{{rvit_template.author_name}}',
+    author='{{cookiecutter.author_name}}',
     author_email='',
     # packages=find_packages('src', exclude=['ez_setup']),
     # package_dir={'': 'src'},
-    namespace_packages=['{{rvit_template.project_slug}}'],
-    license='{{rvit_template.license}}',
-    packages=['{{rvit_template.project_slug}}'],
+    namespace_packages=['{{cookiecutter.project_slug}}'],
+    license='{{cookiecutter.license}}',
+    packages=['{{cookiecutter.project_slug}}'],
     install_requires=[
         'rvit @ git+https://github.com/flaviusb/rvit',
         'kivy @ git+https://github.com/flaviusb/kivy.git@change_check_for_cython',
